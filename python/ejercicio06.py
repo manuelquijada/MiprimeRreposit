96226e8bd83e8bb7d2b3328f1d3276d74bcd7266
manuelquijada <qg0001032026@unab.edu.sv>
2026-04-06 18:14:23 -0600

    Ejercicio  6: separar nombre con split

---
+++ python/ejercicio06.py
@@ -0,0 +1,9 @@
+# Pido el nombre completo
+nombre = input("Escribe tu nombre completo: ")
+
+# Separo el nombre en partes
+partes = nombre.split()
+
+# Muestro cada palabra en una línea
+for palabra in partes:
+    print(palabra)
