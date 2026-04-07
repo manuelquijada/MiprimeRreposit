c0f6e61932e4cea4c0e6483c4249ac93bc9223e4
manuelquijada <qg0001032026@unab.edu.sv>
2026-04-06 18:43:25 -0600

    Ejercicio 10: verificar punto final

---
+++ python/ejercicio10.py
@@ -0,0 +1,8 @@
+# Pido una frase
+frase = input("Escribe una frase: ")
+
+# Verifico si termina con punto
+if frase.endswith("."):
+    print("Termina con punto")
+else:
+    print("No termina con punto")
