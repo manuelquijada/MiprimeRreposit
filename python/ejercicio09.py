d216e3a7540583ac51a1dcc45d1f0eb35d4455a2
manuelquijada <qg0001032026@unab.edu.sv>
2026-04-06 18:42:20 -0600

    Ejercicio 09: verificar inicio con hola

---
+++ python/ejercicio09.py
@@ -0,0 +1,8 @@
+# Pido una frase
+frase = input("Escribe una frase: ")
+
+# Verifico si empieza con Hola
+if frase.startswith("Hola"):
+    print("Sí empieza con Hola")
+else:
+    print("No empieza con Hola")
