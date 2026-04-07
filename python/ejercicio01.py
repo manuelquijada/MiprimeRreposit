4173b27bcd01c63043c4aabcba1849b36649ef6e
manuelquijada <qg0001032026@unab.edu.sv>
2026-04-06 18:05:31 -0600

    Ejercicio 1 : nombre en mayúsculas, munúsculas y longitud

---
+++ python/ejercicio01.py
@@ -0,0 +1,9 @@
+# Pido el nombre completo
+nombre = input("Escribe tu nombre completo: ")
+
+# Muestro el nombre en diferentes formas
+print("En mayúsculas:", nombre.upper())
+print("En minúsculas:", nombre.lower())
+
+# Muestro cuántos caracteres tiene
+print("Cantidad de caracteres:", len(nombre))
