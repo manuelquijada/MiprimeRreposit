8951230fde4f45237fb146cad09938636b6c6d6b
manuelquijada <qg0001032026@unab.edu.sv>
2026-04-06 18:07:43 -0600

    Ejercicio 2: frase en mayúsculas y minñusculas

---
+++ python/ejercicio02.py
@@ -0,0 +1,7 @@
+# Pido una frase
+frase = input("Escribe una frase: ")
+
+# Muestro la frase original y cambiada
+print("Original:", frase)
+print("Mayúsculas:", frase.upper())
+print("Minúsculas:", frase.lower())
