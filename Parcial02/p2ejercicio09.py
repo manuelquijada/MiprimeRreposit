91cfdfe233e82b5ec199cba2ae8dd7ba54a614ef
manuelquijada <qg0001032026@unab.edu.sv>
2026-04-11 16:08:15 -0600

    Fundamentos de programacion Parcial 2 ejercicio 9 reemplazar texto y convertir a mayusxculas

---
+++ Parcial02/p2ejercicio09.py
@@ -0,0 +1,10 @@
+# ejercicio 9
+texto = "Any time y Anytime"
+
+# reemplazo el texto solicitado
+cambio = texto.replace("Any time", "Always")
+
+# convierto a mayusculas
+mayus = cambio.upper()
+
+print(mayus)
