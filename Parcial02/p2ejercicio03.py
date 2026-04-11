8c22cffe689e8ec99bd8c42fa4a16ac1dab42f4f
manuelquijada <qg0001032026@unab.edu.sv>
2026-04-11 15:54:17 -0600

    Funadamentos de programacion Parcial 2 ejercicio 3 quitar prefijo y pasar a mayusculas

---
+++ Parcial02/p2ejercicio03.py
@@ -0,0 +1,10 @@
+# ejercicio 3
+texto = "ING. Manuel"
+
+# elimino el prefijo
+sin_prefijo = texto.removeprefix("ING. ")
+
+# convierto todo a mayusculas
+resultado = sin_prefijo.upper()
+
+print(resultado)
