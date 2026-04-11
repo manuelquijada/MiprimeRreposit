dda47793779e113763f311eb10b7bb01578693e8
manuelquijada <qg0001032026@unab.edu.sv>
2026-04-11 16:00:15 -0600

    Fundamentos de programacion Parcial 2 ejercicio 5 invertir mayusculas y alinear texto

---
+++ Parcial02/p2ejercicio05.py
@@ -0,0 +1,10 @@
+# ejercicio 5
+texto = "pYTHON"
+
+# invierto mayusculas y minusculas
+invertido = texto.swapcase()
+
+# alineo el texto a la izquierda y relleno con *
+resultado = invertido.ljust(15, "*")
+
+print(resultado)
