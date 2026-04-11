31e337c95ec9ec5eb650ee5e6a71f818ce5088ee
manuelquijada <qg0001032026@unab.edu.sv>
2026-04-11 16:06:13 -0600

    Fundamentos de programacion Parcial 2 ejercicio 8 contar letras y separar lineas

---
+++ Parcial02/p2ejercicio08.py
@@ -0,0 +1,14 @@
+# ejercicio 8
+# bloque de texto en 3 lineas
+texto = """la luna brilla
+en la mañana
+y canta el alma"""
+
+# cuento cuantas veces aparece la letra a
+cantidad = texto.count("a")
+
+# separo cada linea en una lista
+lineas = texto.splitlines()
+
+print(cantidad)
+print(lineas)
