84294cbb4e60ddf88c469813ad7cd9951ad3795f
manuelquijada <qg0001032026@unab.edu.sv>
2026-04-11 16:14:53 -0600

    Fundamentos de programacion Parcial 2 ejercicio 11 limpiar y centar texto

---
+++ Parcial02/p2ejercicio11.py
@@ -0,0 +1,10 @@
+# ejercicio 11
+texto = "  el nido matinal  "
+
+# limpio espacios y pongo primeras letras en mayuscula
+nuevo = texto.strip().title()
+
+# centro el texto con guiones
+centrado = nuevo.center(30, "-")
+
+print(centrado)
