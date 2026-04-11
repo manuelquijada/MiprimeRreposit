0dc94dab0454e579035097fd37c8eb9abca15b5b
manuelquijada <qg0001032026@unab.edu.sv>
2026-04-11 15:52:15 -0600

    Fundamentos de programación Parcial2 ejercicio 2 convertir texto y reemplazar palabra

---
+++ Parcial02/p2ejercicio02.py
@@ -0,0 +1,11 @@
+# ejercicio 2
+# guardo el texto
+texto = "Manuel"
+
+# pongo la primera letra de cada palabra en mayuscula
+nuevo = texto.title()
+
+# cambio la palabra nombre por apellido
+cambio = nuevo.replace("Manuel", "Quijada")
+
+print(cambio)
