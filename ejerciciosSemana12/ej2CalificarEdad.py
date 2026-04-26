6c8cdbbb929c505a5a0e2c73ef1db3c3c3bac873
manuelquijada <qg0001032026@unab.edu.sv>
2026-04-26 17:55:56 -0600

    Calificacion de edad

---
+++ ejerciciosSemana12/ej2CalificarEdad.py
@@ -0,0 +1,9 @@
+# Calificacion de numero.
+edad = int(input("Ingresa tu edad: "))
+
+if edad < 18:
+    print("Menor de edad")
+elif edad < 60:
+    print("Mayor de edad")
+else:
+    print("Adulto mayor")
