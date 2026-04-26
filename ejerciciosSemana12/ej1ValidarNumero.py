0a04ca83cbb94b80aab5c382191b71d01de09727
manuelquijada <qg0001032026@unab.edu.sv>
2026-04-26 17:54:00 -0600

    Validar si numero es negatico positivo o cero

---
+++ ejerciciosSemana12/ej1ValidarNumero.py
@@ -0,0 +1,10 @@
+# validar si numero es positivo negativo o 0
+
+numero = int(input("Ingresa un número: "))
+
+if numero > 0:
+    print("Es positivo")
+elif numero < 0:
+    print("Es negativo")
+else:
+    print("Es cero")
