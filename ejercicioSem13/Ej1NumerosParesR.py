a5621ed8f6daa1c4f6d69a4b4c850fc985d464e5
manuelquijada <qg0001032026@unab.edu.sv>
2026-04-26 19:17:24 -0600

    Numeros pares en rango

---
+++ ejercicioSem13/Ej1NumerosParesR.py
@@ -0,0 +1,11 @@
+# Numeros pares en rango .
+
+while True:
+    n = int(input("Ingrese un número, 0 para salir: "))
+
+    if n == 0:
+        break
+
+    for i in range(1, n + 1):
+        if i % 2 == 0:
+            print(i)
