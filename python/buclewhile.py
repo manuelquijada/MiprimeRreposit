3cd776b46e47df5b0e45a4b39df5dbd92e2e871b
manuelquijada <qg0001032026@unab.edu.sv>
2026-04-26 18:47:29 -0600

    ejemplos python

---
+++ python/buclewhile.py
@@ -0,0 +1,31 @@
+# si tenemos una variable y necesitamos comprobar
+
+clima = "caliente"  ##  clima por defecto
+# entrada = input("¿Cómo está el clima? ")
+## print("El clima es: ", entrada)
+
+# para igualar un valor vamos a tomar ==
+numeroComparar = False
+numer2 = 50
+
+if numeroComparar == False:
+    print("debes de trabajar para comprar una terreno para que tus nietos  constr.")
+else:
+    print("Debes cuidar tus rodillas")
+
+## if sera nuestro camino ninja
+## else es por defecto en caso que el if no sea true
+
+# and -> dos true
+# or  ->  si tenemos un solo true
+
+if numer2 > 24 and numer2 < 30:
+    print("El numero es mayor a 24 y menor a 30")
+elif numer2 > 30 and numer2 < 35:
+    print("El numero es mayor a 30")
+elif numer2 > 35:
+    print("El numero es mayor a 35 cliente vip")
+else:
+    print("El numero es menor a 24")
+
+# verifica un camino si este camino no esta deduce una opcion por decto
