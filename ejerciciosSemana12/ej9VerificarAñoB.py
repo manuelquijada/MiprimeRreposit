ba858196ce9619c1c6a15b203f27681bb7f2963c
manuelquijada <qg0001032026@unab.edu.sv>
2026-04-26 18:40:14 -0600

    Verificar si un año es bisisesto

---
+++ ejerciciosSemana12/ej9VerificarAñoB.py
@@ -0,0 +1,8 @@
+# Verificar  si un año es bisiesto.
+
+anio = int(input("Ingresa un año: "))
+
+if (anio % 4 == 0 and anio % 100 != 0) or (anio % 400 == 0):
+    print("Es año bisiesto")
+else:
+    print("No es bisiesto")
