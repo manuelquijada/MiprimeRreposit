55b8dc36a5371ed1e4b92ebc32928cc73fd005b5
manuelquijada <qg0001032026@unab.edu.sv>
2026-04-26 18:12:47 -0600

    Mostrar dia de la semana segun numero

---
+++ ejerciciosSemana12/ej6Diadesemana.py
@@ -0,0 +1,20 @@
+# Mostrar Dia de samana segun numero.
+
+dia = int(input("Ingresa un número del 1 al 7: "))
+
+if dia == 1:
+    print("Lunes")
+elif dia == 2:
+    print("Martes")
+elif dia == 3:
+    print("Miércoles")
+elif dia == 4:
+    print("Jueves")
+elif dia == 5:
+    print("Viernes")
+elif dia == 6:
+    print("Sábado")
+elif dia == 7:
+    print("Domingo")
+else:
+    print("Número fuera de rango")
